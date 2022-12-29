3931efe5b5889fbe64735c42a666c4c420710bc2
Jedd Dryden <jedd.dryden@gmail.com>
2022-12-30 01:41:08 +1100

    Update parse.rs

--- src/parse.rs
+++ src/parse.rs
@@ -42,15 +42,35 @@ impl Csv {
 mod tests {
     use super::*;
 
+    fn get_raw(name: &str) -> String {
+        let path = format!("files/{}.csv", name);
+
+        read_to_string(path).unwrap()
+    }
+
     #[test]
-    fn parse() {
-        let result = Csv::parse("super,duper\r\nepic,gamer\r\nnew,lines", true);
+    fn without_headers() {
+        let result = Csv::parse(&get_raw("simple"), false);
 
         println!("{:?}", result);
     }
 
     #[test]
-    fn parse_file() {
+    fn with_headers() {
+        let result = Csv::parse(&get_raw("simple"), true);
+
+        println!("{:?}", result);
+    }
+
+    #[test]
+    fn file_without_headers() {
+        let result = Csv::parse_file("files/simple.csv", false);
+
+        println!("{:?}", result);
+    }
+
+    #[test]
+    fn file_with_headers() {
         let result = Csv::parse_file("files/simple.csv", true);
 
         println!("{:?}", result);
